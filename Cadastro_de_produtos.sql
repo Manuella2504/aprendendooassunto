c6f0383488a4dd4822508842c14e015d19762fb5
Manuella <manuella.rosario@ba.estudante.senai.br>
2025-08-20 17:28:13 -0300

    Add files via upload

---
+++ Cadastro_de_produtos.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE atividadeprocessual;
+USE atividadeprocessual;
+
+create table usuarios(
+id int not null auto_increment primary key,
+nomeProd varchar(100) not null,
+PrecoCompra int not null,
+Quantidade int not null,
+Datacompra int not null
+);
+
+select * from usuarios
+
+
+
+
+
+
+
+
+
+
+
+
+
